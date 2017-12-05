27462a995079dc7051cdc219b3ae686c2cc62102
josephmkotvis <josephmkotvis@gmail.com>
2017-12-05 11:23:10 -0600

    Removed the class TestSetAddIndex and moved the method inside to the renamed TestAdding class. Created a new class called TestReplacing

---
+++ TestAddLength/TestAdding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAddLength
+{
+    [TestClass]
+    public class TestAddToLength
+    {
+        [TestMethod]
+        public void Add1toLength()    
+        {
+            Program program = new Program();
+            int currentListLength = 0;
+            int result = program.addListLength(currentListLength);
+            Assert.AreEqual(result, 1); 
+        }
+
+    }
+}
